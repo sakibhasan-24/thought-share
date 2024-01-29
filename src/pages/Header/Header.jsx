1335b1a7c792d49bef357fabbbda16d87b9df310
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-01-29 11:22:46 +0600

    create header part

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,8 +1,11 @@
 import { Button, Navbar, TextInput } from "flowbite-react";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 export default function Header() {
+  const location = useLocation();
+  const path = location.pathname;
   return (
     <Navbar className="border-b-2 border-sky-200 sm:p-4">
       <Link className="font-bold text-xs sm:text-xl " to="/">
@@ -18,10 +21,30 @@ export default function Header() {
           placeholder="search..."
           rightIcon={AiOutlineSearch}
         />
-        <Button className="rounded-full sm:hidden">
+        <Button className="rounded-full sm:hidden w-8 h-8">
           <AiOutlineSearch />
         </Button>
       </form>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-8 h-8 sm:w-10 sm:h-10">
+          <FaMoon />
+        </Button>
+        <Link to="/signup">
+          <Button gradientDuoTone="greenToBlue">SignUp</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link as={"div"} active={location.pathname === "/"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link as={"div"} active={location.pathname === "/about"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link as={"div"} active={location.pathname === "/projects"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
