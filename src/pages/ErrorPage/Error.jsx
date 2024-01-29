5f8be380021ec7582f82fe54f57d0d55fc2a940c
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-01-29 11:29:23 +0600

    handle error page

--- src/pages/ErrorPage/Error.jsx
+++ src/pages/ErrorPage/Error.jsx
@@ -1,5 +1,28 @@
+import { Button } from "flowbite-react";
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Error() {
-  return <div>Error</div>;
+  const navigate = useNavigate();
+  const handleNavigate = (direction) => {
+    navigate(direction);
+  };
+  return (
+    <div className="h-[100vh] flex flex-col items-center justify-center">
+      <div>
+        <h1 className="sm:text-6xl font-semibold">
+          <span className="text-red-700">404</span> error!
+        </h1>
+      </div>
+      <div className="flex items-center justify-center gap-6 my-6">
+        <Link to="/">
+          <Button>Go home</Button>
+        </Link>
+
+        <Button onClick={() => handleNavigate(-1)} gradientMonochrome="failure">
+          Go back
+        </Button>
+      </div>
+    </div>
+  );
 }
