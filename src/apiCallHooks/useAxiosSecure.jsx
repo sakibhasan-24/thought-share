d2dd290bbd2ba8a58ff4e61ca3d22a8e4cea2792
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-02-03 01:26:14 +0600

    create post functionality

--- src/apiCallHooks/useAxiosSecure.jsx
+++ src/apiCallHooks/useAxiosSecure.jsx
@@ -5,26 +5,32 @@ export default function useAxiosSecure() {
     baseURL: "http://localhost:5000",
   });
 
-  axiosSecure.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("token");
+      // console.log(token);
+      if (token) {
+        config.headers.authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      alert("error");
+      return Promise.reject(error);
     }
-    return config;
-  });
-  (error) => {
-    return Promise.reject(error);
-  };
+  );
 
-  axiosSecure.interceptors.response.use((response) => {
-    return response;
-  }),
+  axiosSecure.interceptors.response.use(
+    (response) => {
+      return response;
+    },
     (error) => {
       if (error.response.status === 401 || error.response.status === 403) {
-        alert("your are 401");
+        console.log("Unauthorized", error);
       }
       return Promise.reject(error);
-    };
+    }
+  );
 
   return axiosSecure;
 }
--- src/pages/Post/CreatePost.jsx
+++ src/pages/Post/CreatePost.jsx
@@ -1,20 +1,27 @@
-import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import { Button, FileInput, Select, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import useAxiosPublic from "../../apiCallHooks/useAxiosPublic";
 import useAxiosSecure from "../../apiCallHooks/useAxiosSecure";
+import useAuth from "../../hook/useAuth";
+import Swal from "sweetalert2";
 export default function CreatePost() {
+  const { user } = useAuth();
   const [imageFile, setImageFile] = useState(null);
+
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
   const image_key = import.meta.env.VITE_IMAGE_API_KEY;
   //   console.log(image_key);
   const image_hosting = `https://api.imgbb.com/1/upload?key=${image_key}`;
   const axiosPublic = useAxiosPublic();
   const axiosSecure = useAxiosSecure();
-  const handlePost = async (e) => {
-    e.preventdefault();
-  };
+  //   const handlePost = async (e) => {
+  //     e.preventdefault();
+  //   };
   //   console.log(imageFile);
+
   const handleUploadImage = async () => {
     const image = { image: imageFile };
     const res = await axiosPublic.post(image_hosting, image, {
@@ -22,12 +29,40 @@ export default function CreatePost() {
         "Content-Type": "multipart/form-data",
       },
     });
+    setFormData({ ...formData, image: res.data.data.display_url });
   };
 
+  //   console.log(formData);
+
+  const handleSubmitForm = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const userPostData = {
+      ...formData,
+      userEmail: user?.email,
+      createdAt: new Date(),
+      privacy: "public",
+    };
+    const res = await axiosSecure.post("/create-post", userPostData);
+    // console.log(res);
+    if (res.data.insertedId) {
+      Swal.fire({
+        icon: "success",
+        title: "Post created successfully",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+    setLoading(false);
+  };
+  //   console.log(formData?.image);
+  const isButtonDisabled =
+    formData?.image === null || formData?.image === undefined ? true : false;
+  //   console.log(isButtonDisabled);
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-3xl font-bold mb-3 text-center">Create Post</h1>
-      <form className="flex flex-col gap-4" onSubmit={handlePost}>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmitForm}>
         <div className="flex flex-col gap-6 sm:flex-row justify-between">
           <TextInput
             type="text"
@@ -35,8 +70,15 @@ export default function CreatePost() {
             placeholder="title"
             id="title"
             required
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
           ></TextInput>
-          <Select>
+          <Select
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+          >
             <option value="uncategorized">Select a Category</option>
             <option value="technology">Technology</option>
             <option value="lifestyle">Lifestyle</option>
@@ -74,8 +116,11 @@ export default function CreatePost() {
           theme="snow"
           placeholder="write relevent things"
           className="h-60 mb-16"
+          onChange={(value) => setFormData({ ...formData, content: value })}
         />
-        <Button type="submit">Upload Post</Button>
+        <Button disabled={isButtonDisabled} type="submit">
+          {loading ? <Spinner /> : "create A post"}
+        </Button>
       </form>
     </div>
   );
