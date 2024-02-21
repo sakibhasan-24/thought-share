155431c61a8e8f500f739cf3f6c10b5185fed037
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-02-21 23:05:05 +0600

    implement different mode

--- src/components/GoogleSignUpButton.jsx
+++ src/components/GoogleSignUpButton.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
+import app from "../firebase/firebase.config";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  loginStart,
+  loginSuccess,
+  loginFailure,
+} from "../redux/store/userSlice";
+import { useDispatch } from "react-redux";
+import useAxiosPublic from "../hook/useAxiosPublic";
 export default function GoogleSignUpButton() {
+  const dispatch = useDispatch();
+
+  const axiosPublic = useAxiosPublic();
+  const googleProvider = new GoogleAuthProvider();
+  const auth = getAuth(app);
+  const [loading, setLoading] = useState(false);
+  const handleGoogleSignUp = async () => {
+    setLoading(true);
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      console.log(result.user);
+      // save in db
+      const res = await axiosPublic.post("/api/users/google", {
+        userName: result?.user.displayName,
+        email: result?.user.email,
+        profilePicture: result?.user.photoURL,
+      });
+      dispatch(loginSuccess(res.data));
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <button
+      onClick={handleGoogleSignUp}
       className="bg-blue-600 text-white px-4 py-2 rounded-lg w-full hover:bg-blue-700"
       type="button"
     >
-      Continue With Google{" "}
+      {loading ? "Loading..." : "Sign up with Google"}
     </button>
   );
 }
--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,56 +1,63 @@
-import { Avatar, Dropdown, Navbar } from "flowbite-react";
-import React from "react";
+import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
   const location = useLocation();
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
+  const [themeNumber, setThemeNumber] = useState(0);
+  const ariaLevel = ["Default", "Retro", "Cyberpunk", "Valentine", "Aqua"];
+  const value = ["default", "retro", "cyberpunk", "valentine", "aqua"];
+  const handleTheme = () => {
+    setThemeNumber((themeNumber) => themeNumber + 1);
+    if (themeNumber === 4) setThemeNumber(0);
+  };
   return (
     <Navbar fluid rounded className="bg-slate-900 p-6 rounded-lg">
       <Link to="/" className="font-semibold flex items-center">
-        <span>Thought</span>
+        <span className="text-neutral-content text-2xl">Thought</span>
         <span className="text-orange-800 sm:text-3xl">Share</span>
       </Link>
 
       {/* dynamic */}
-      <div className="flex md:order-2">
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar
-              alt="User settings"
-              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-              rounded
-            />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">
-              name@flowbite.com
-            </span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown>
-        <Navbar.Toggle />
-      </div>
-      <div>
-        {/* <form className="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered sm:w-24 md:w-auto"
-          />
-        </form> */}
-      </div>
+      {currentUser && (
+        <div className="flex md:order-2">
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="UserImage"
+                img={currentUser?.profilePicture}
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">Bonnie Green</span>
+              <span className="block truncate text-sm font-medium">
+                {currentUser?.email}
+              </span>
+            </Dropdown.Header>
+            <Dropdown.Item>Dashboard</Dropdown.Item>
+            <Dropdown.Item>Settings</Dropdown.Item>
+            <Dropdown.Item>Earnings</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+          <Navbar.Toggle />
+        </div>
+      )}
+
       <Navbar.Collapse>
-        <Navbar.Link active={location.pathname === "/login"} as={"div"}>
+        <Navbar.Link
+          active={!currentUser && location.pathname === "/login"}
+          as={"div"}
+        >
           <Link className="cursor-pointer text-xl font-bold" to="/login">
-            Login
+            {currentUser ? "LogOut" : "Login"}
           </Link>
         </Navbar.Link>
         <Navbar.Link active={location.pathname === "/projects"} as={"div"}>
@@ -59,6 +66,24 @@ export default function Header() {
           </Link>
         </Navbar.Link>
       </Navbar.Collapse>
+      <input
+        onClick={handleTheme}
+        type="radio"
+        name="theme-buttons"
+        className="btn px-2 theme-controller join-item w-[100px] rounded-md"
+        aria-label={ariaLevel[themeNumber]}
+        value={value[themeNumber]}
+      />
     </Navbar>
   );
+}
+
+{
+  /* <form className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered sm:w-24 md:w-auto"
+          />
+        </form> */
 }
